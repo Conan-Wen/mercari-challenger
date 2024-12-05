70a2bdbfaf9fc1c1f57825e0c4e44425076e80fe
Conan-Wen <w245646037@gmail.com>
2024-12-05 23:21:30 +0900

    Like履歴から人物性格を分析するAPI

--- backend/src/modules/like/like.service.ts
+++ backend/src/modules/like/like.service.ts
@@ -3,6 +3,7 @@ import { Repository } from 'typeorm';
 import { likeConstants } from './like.constants';
 import { Like } from './like.entity';
 import { ArticleService } from '../article/article.service';
+import axios from 'axios';
 
 @Injectable()
 export class LikeService {
@@ -27,5 +28,39 @@ export class LikeService {
     like.article = article;
 
     return this.likeRepository.save(like);
+  }
+
+  async analyze(): Promise<{ result: string }> {
+    const likes = await this.likeRepository.find({ relations: ['article'] });
+
+    let promp = '';
+    for (const like of likes) {
+      promp += `title: ${like.article.title}\ndescription: ${like.article.job_detail}\n\n`;
+    }
+
+    const res = await axios.post(
+      'https://api.openai.com/v1/chat/completions',
+      {
+        model: 'gpt-4o',
+        messages: [
+          {
+            role: 'system',
+            content:
+              'ユーザは以下のアルバイトに興味があります。そこから、このユーザはどんな性格かを推測してください。（200程度でまとめてください)\nユーザを呼ばずに、第二人称を使ってください。',
+          },
+          { role: 'user', content: promp },
+        ],
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+      },
+    );
+
+    console.log(res.data.choices[0].message.content);
+
+    return { result: res.data.choices[0].message.content };
   }
 }
